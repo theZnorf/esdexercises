d556113963b0d06a2aeb1d564fd93d39e7adc93d
franz <franz.profelt@gmail.com>
2015-10-03 20:18:39 +0200

    SQE: - added literature - added Q&A file - added sourcecode header to Messages - recreated JavaDoc

--- SQE/SQE01-Profelt-Triangle1st/src/at/fhhagenberg/sqe/exercise1/Messages.java
+++ SQE/SQE01-Profelt-Triangle1st/src/at/fhhagenberg/sqe/exercise1/Messages.java
@@ -1,8 +1,19 @@
+/**
+ * @author Franz Profelt
+ * @id s1410567019
+ */
+
 package at.fhhagenberg.sqe.exercise1;
 
 import java.util.MissingResourceException;
 import java.util.ResourceBundle;
 
+/**
+ * Generated Class for easy resource handling
+ * 
+ * @author Franz Profelt
+ *
+ */
 public class Messages {
     private static final String BUNDLE_NAME = "at.fhhagenberg.sqe.exercise1.messages"; //$NON-NLS-1$
 
