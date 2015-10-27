de3624a8736c915bccca187e9c026ad2d2586f40
franz <franz.profelt@gmail.com>
2015-10-27 09:55:57 +0100

    SQE: - added new project for exercise 05

---
+++ SQE/SQE05-Profelt_RingBufferTest/src/at/fhhagenberg/sqe/exercise5/AllTests.java
@@ -0,0 +1,15 @@
+/**
+ * @author Franz Profelt
+ * @id s1410567019
+ */
+package at.fhhagenberg.sqe.exercise5;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ EmptyRingBufferTest.class, RingBufferTest.class })
+public class AllTests {
+
+}
---
+++ SQE/SQE05-Profelt_RingBufferTest/src/at/fhhagenberg/sqe/exercise5/EmptyRingBufferTest.java
@@ -0,0 +1,68 @@
+/**
+ * @author Franz Profelt
+ * @id s1410567019
+ */
+package at.fhhagenberg.sqe.exercise5;
+
+import static org.junit.Assert.*;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+public class EmptyRingBufferTest {
+
+    static final int capacity = 10;
+    
+    // Member
+    RingBuffer<Integer> buffer;
+    
+    @Rule
+    public ExpectedException exception = ExpectedException.none();
+    
+    @Before
+    public void setUp() throws Exception {
+        // create buffer
+        buffer = new RingBuffer<>(capacity);
+    }
+
+    @Test
+    public void testIsEmpty() {
+        // check is empty
+        assertTrue(buffer.isEmpty());
+    }
+
+    @Test
+    public void testSize() {
+        // check size
+        assertEquals(0, buffer.size());
+    }
+
+    @Test
+    public void testDequeue() {
+        // set expected exception
+        exception.expect(RuntimeException.class);
+        exception.expectMessage("Ring buffer underflow");
+        buffer.dequeue();
+    }
+
+    @Test
+    public void testIterator() {
+        Iterator<Integer> iter = buffer.iterator();
+        assertNotNull(iter);
+        
+        // check has next
+        assertFalse(iter.hasNext());
+        
+        // set expected exception
+        exception.expect(NoSuchElementException.class);
+        // call next
+        iter.next();
+    }
+
+}
