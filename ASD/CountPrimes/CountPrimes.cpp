1a6ef287d7485b9f17ae6165e78c0455337e9e9b
Franz Profelt <franz.profelt@gmail.com>
2015-11-16 17:08:06 +0100

    ASD: - added initial CountPrimes project

---
+++ ASD/CountPrimes/CountPrimes.cpp
@@ -0,0 +1,119 @@
+#include <iostream>
+#include <algorithm>
+#include <iterator>
+#include <numeric>
+#include <functional>
+#include <vector>
+#include <string>
+#include <limits>
+#include <cmath>
+#include <random>
+#include <thread>
+#include <chrono>
+
+using namespace std;
+using namespace std::chrono;
+using namespace std::placeholders;
+
+size_t const smallN = 1000;
+size_t const largeN = 250000;
+
+using Number = int;
+using NumberVec = vector<Number>;
+
+bool IsPrime(Number const x)
+{
+    if (x <= 1)
+    {
+        return false;
+    }
+    if (x == 2)
+    {
+        return true;
+    }
+    if (x % 2 == 0)
+    {
+        return false;
+    }
+    auto max = (Number) ceil(sqrt(double(x)));
+    for (Number d = 3; d <= max; d += 2)
+    {
+        if (x % d == 0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+size_t CountPrimes(NumberVec const &vec)
+{
+    return count_if(vec.begin(), vec.end(), IsPrime);
+}
+
+size_t ParallelCountPrimes(NumberVec const &vec, unsigned int const nrThreads)
+{
+    size_t result = 0;
+    auto begin = vec.cbegin();
+    auto end = vec.cend();
+    thread t([begin, end, &result]
+             {
+                 result = count_if(begin, end, IsPrime);
+             });
+    t.join();
+    return result;
+    // TODO: use multiple threads instead of a single one
+}
+
+void InitRandom(NumberVec &vec, size_t const nr)
+{
+    auto rgen = std::bind(uniform_int_distribution<Number>(0, numeric_limits<Number>::max()), default_random_engine());
+    vec.reserve(nr);
+    generate_n(back_inserter(vec), nr, rgen);
+}
+
+void TestFunctional(unsigned int maxThreads)
+{
+    NumberVec vec;
+    InitRandom(vec, smallN);
+    cout << "*** Functional tests ***" << endl;
+    cout << "CountPrimes: " << CountPrimes(vec) << endl;
+    for (unsigned int i = 1; i <= maxThreads; i++)
+    {
+        cout << "ParallelCountPrimes(" << i << "): " << ParallelCountPrimes(vec, i) << endl;
+    }
+    cout << endl;
+}
+
+double MeasureRuntime(std::function<size_t(NumberVec const &)> f, NumberVec const &vec, size_t &result)
+{
+    auto start = high_resolution_clock::now();
+    result = f(vec);
+    return duration<double>(high_resolution_clock::now() - start).count();
+}
+
+void TestRuntime(unsigned int maxThreads)
+{
+    NumberVec vec;
+    cout << "*** Runtime tests ***" << endl;
+    InitRandom(vec, largeN);
+    size_t result = 0;
+    double time1 = MeasureRuntime(CountPrimes, vec, result);
+    cout << "CountPrimes: " << result << "; Runtime: " << time1 << endl;
+    for (unsigned int i = 1; i <= maxThreads; i++)
+    {
+        double time = MeasureRuntime(std::bind(ParallelCountPrimes, _1, i), vec, result);
+        cout << "ParallelCountPrimes(" << i << "): " << result << "; Runtime: " << time << "; Speedup: " <<
+        time1 / time << endl;
+    }
+}
+
+int main()
+{
+    unsigned int maxThreads = thread::hardware_concurrency() + 2;  // test up to 2 threads more than CPU cores available
+    TestFunctional(maxThreads);
+#ifdef NDEBUG
+    TestRuntime(maxThreads);
+#endif
+    return 0;
+}
