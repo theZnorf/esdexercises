a4ab49dae0e39746ec8ebbe42534d9feccf59536
Franz Profelt <franz.profelt@gmail.com>
2015-11-16 13:39:16 +0100

    ASD: - added initial Median project

---
+++ ASD/Median/Median.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <algorithm>
+#include <cassert>
+#include <vector>
+#include <memory>
+#include <functional>
+#include <iterator>
+
+using namespace std;
+
+class Object {
+public:
+  explicit Object(double value) : mValue(value) {}
+  double GetValue() const { return mValue; }
+private:
+  double mValue;
+};
+
+using Objects = vector<unique_ptr<Object>>;
+
+double Median(Objects const &objects) {
+  assert(!objects.empty());
+  vector<double> values;
+  // Alternative 1: Explizite Schleife
+  for (auto &obj : objects) {
+    values.push_back(obj->GetValue());
+  }
+  // Alternative 2: transform + mem_fn/bind
+  // ???
+  // Alternative 3: transform + Lambda
+  // ???
+  size_t nr = objects.size();
+  size_t m = nr / 2;
+  partial_sort(values.begin(), values.begin() + m + 1, values.end());
+  if (nr % 2 == 1) {
+    return values[m];
+  }
+  return (values[m - 1] + values[m]) / 2;
+}
+
+int main() {
+  Objects objects;
+  objects.emplace_back(new Object(5));
+  objects.emplace_back(new Object(2));
+  objects.emplace_back(new Object(10));
+  objects.emplace_back(new Object(8));
+  objects.emplace_back(new Object(1));
+  cout << "Median: " << Median(objects) << endl;    // 5
+  return 0;
+}
