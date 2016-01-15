3800ee2a2f8da334e90b55d78243dcd499ce3fc7
Franz Profelt <franz.profelt@gmail.com>
2016-01-15 13:05:58 +0100

    ASD:
    - implemented new and delete operator for expressions

--- ASD/CalculatorSmartPtr/Expression.cpp
+++ ASD/CalculatorSmartPtr/Expression.cpp
@@ -4,23 +4,41 @@
 
 #include "Expression.h"
 #include "Number.h"
+#include "BinaryExpr.h"
+#include "UnaryExpr.h"
 
 void *Expression::operator new(size_t size)
 {
-    // check size
     if (size == sizeof(Number))
+    {
         return SinglePool<Number>::malloc();
-        // TODO: Handle unary and binary expression
+    }
+    else if (size == sizeof(AddExpr)) // same size as other binary expressions
+    {
+        return SinglePool<AddExpr>::malloc();
+    }
+    else if (size == sizeof(NegativeExpr)) // same size as other unary expressions
+    {
+        return SinglePool<NegativeExpr>::malloc();
+    }
     else
         return ::operator new(size);
 }
 
 void Expression::operator delete(void *ptr, size_t size)
 {
-    // check size
     if (size == sizeof(Number))
+    {
         SinglePool<Number>::free(ptr);
-        // TODO: Handle unary and binary expression
+    }
+    else if (size == sizeof(AddExpr)) // same size as other binary expressions
+    {
+        SinglePool<AddExpr>::free(ptr);
+    }
+    else if (size == sizeof(NegativeExpr)) // same size as other unary expressions
+    {
+        SinglePool<NegativeExpr>::free(ptr);
+    }
     else
         ::operator delete(ptr);
 }
